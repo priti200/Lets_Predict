b4b86297652d8f88dd72e57c8fd0adaab406b99f
Avishek8136 <avishekrauniyar07@gmail.com>
2025-10-04 11:20:29 +0530

    feat: Initial implementation of GeoClima AI

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,37 +1,36 @@
-import React, { useState, useEffect, useRef } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, FlyToInterpolator } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_API_KEY;
 
 const MapComponent = ({ position: externalPosition }) => {
-  const [viewState, setViewState] = useState({
+  const [viewport, setViewport] = useState({
     longitude: 75.7804, // Calicut
     latitude: 11.2588,
-    zoom: 10
+    zoom: 10,
+    width: '100%',
+    height: '100%'
   });
-  const mapRef = useRef();
 
   useEffect(() => {
-    const handleMove = () => {
-        if (mapRef.current) {
-            const { lng, lat } = mapRef.current.getMap().getCenter();
-            setViewState(prevState => ({...prevState, longitude: lng, latitude: lat }));
-        }
+    const flyToPosition = (lat, lon) => {
+        setViewport(v => ({
+            ...v,
+            longitude: lon,
+            latitude: lat,
+            zoom: 12,
+            transitionDuration: 2000,
+            transitionInterpolator: new FlyToInterpolator(),
+        }));
     };
 
-    if (mapRef.current) {
-        mapRef.current.on('move', handleMove);
-    }
-
     if (externalPosition) {
-        mapRef.current?.flyTo({ center: [externalPosition[1], externalPosition[0]], duration: 2000, zoom: 12 });
+        flyToPosition(externalPosition[0], externalPosition[1]);
     } else {
         navigator.geolocation.getCurrentPosition(
             (pos) => {
-                const newPos = { longitude: pos.coords.longitude, latitude: pos.coords.latitude };
-                setViewState(prevState => ({...prevState, ...newPos, zoom: 12}));
-                mapRef.current?.flyTo({ center: [newPos.longitude, newPos.latitude], duration: 2000 });
+                flyToPosition(pos.coords.latitude, pos.coords.longitude);
             },
             () => {
                 console.log("Geolocation failed, defaulting to Calicut.");
@@ -39,24 +38,18 @@ const MapComponent = ({ position: externalPosition }) => {
             { enableHighAccuracy: true }
         );
     }
-
-    return () => {
-        if (mapRef.current) {
-            mapRef.current.off('move', handleMove);
-        }
-    };
   }, [externalPosition]);
 
   return (
     <ReactMapGL
-      {...viewState}
-      ref={mapRef}
-      onMove={evt => setViewState(evt.viewState)}
-      style={{ width: '100%', height: '100%' }}
+      {...viewport}
+      onViewportChange={newViewport => setViewport(v => ({...v, ...newViewport}))}
       mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
-      mapboxAccessToken={MAPBOX_TOKEN}
+      mapboxApiAccessToken={MAPBOX_TOKEN}
     >
-        <Marker longitude={viewState.longitude} latitude={viewState.latitude} color="red" />
+        <Marker longitude={viewport.longitude} latitude={viewport.latitude} offsetLeft={-20} offsetTop={-10}>
+            <div style={{color: 'red', fontSize: '24px'}}>📍</div>
+        </Marker>
     </ReactMapGL>
   );
 };
